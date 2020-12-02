03756ff2f102f21f02928a84726513e71dda392f
Harshad Yewale <harshadyewale125@gmail.com>
2020-12-02 11:17:17 +0530

    Initial commit

---
+++ Task1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>   
+        <h1>Calculate the umber of servers on the basis of Load </h1>
+        <!-- Basic HTML Form -->
+    <form method="post" action="index.php">
+      Enter The number of servers  <input type="Number" name="Nservers"><br><br>
+        Load in % at minute 1 <input type="Number" name="Load1"><br><br>
+        Load in % at minute 2 <input type="Number" name="Load2"><br><br>
+        Load in % at minute 3 <input type="Number" name="Load3"><br><br>
+        Load in % at minute 4 <input type="Number" name="Load4"><br><br>
+        Load in % at minute 5 <input type="Number" name="Load5"><br><br>
+        <input type="submit" value="Submit">
+    </form><br><br>
+</body>
+</html>
+<?php
+        // Catch user input Here
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $Nservers =$_POST["Nservers"];
+        $load1 = $_POST["Load1"];
+        $load2 = $_POST["Load2"];
+        $load3 = $_POST["Load3"];
+        $load4 = $_POST["Load4"];
+        $load5 = $_POST["Load5"];
+        $load= array($load1, $load2, $load3, $load4, $load5);
+        
+        // Conditional Statment/Actual logic
+        if($load[4]>50){
+            $Nservers = ($Nservers*2) + 1;
+            echo "The Number of servers are: " . $Nservers;
+        }
+        else{
+            $Nservers = ($Nservers/2);
+            echo "The Number of servers after 5min: " . $Nservers;
+        }
+    }
+    ?>
---
+++ Task2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+            <body>
+            <h1>Calculate Maximum Profit</h1><br>
+             <!-- Basic HTML Form -->
+            <form method="post" action="index.php">
+                Enter the quantity of bread <input type="Number" name="bread_quantity"><br><br>
+                Enter the quantity of samosa <input type="Number" name="samosa_quantity"><br><br>
+                Enter the quantity of vada <input type="Number" name="vada_quantity"><br><br>
+                Enter the Price of vadapav <input type="Number" name="vadapav_price"><br><br>
+                Enter the Price of samosapav <input type="Number" name="samosapav_price"><br><br>
+                <input type="submit" value="submit">
+            </form><br><br>
+</body>
+</html>
+    <?php
+        // Catch user input Here
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $bread_quantity = $_POST['bread_quantity'];
+            $samosa_quantity = $_POST['samosa_quantity'];
+            $vada_quantity = $_POST['vada_quantity'];
+            $vadapav_price = $_POST['vadapav_price'];
+            $samosapav_price = $_POST['samosapav_price'];
+
+             // Conditional Statment/Actual logic
+                if($vadapav_price>=$samosapav_price){
+                    $remainbread = $bread_quantity - ($vada_quantity*2);
+                    $vada_profit= $vada_quantity * $vadapav_price;
+                    $samosapav_profit= $samosapav_price* intval($remainbread/2);
+                    $max_profit=$vada_profit+$samosapav_profit;
+                    echo $max_profit;
+                    }
+                else {
+                    $remainbread = $bread_quantity - ($samosa_quantity*2);
+                    $samosapav_profit= $samosa_quantity * $samosapav_price;
+                    $vada_profit= $vadapav_price* intval($remainbread/2);
+                    $max1_profit=$vada_profit+$samosapav_profit;
+                    echo "The maximum profit you can earn is " . $max1_profit. " Rs.";
+                    }
+                }
+    ?>            
+
